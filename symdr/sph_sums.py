c1917f6e6e9757fa23cc29e77a8b7edcae70e1d7
Cosmic47 <cosmicstriker26@gmail.com>
2025-07-18 11:52:23 +0700

    Added exclusive SPH sums

--- symdr/sph_sums.py
+++ symdr/sph_sums.py
@@ -46,6 +46,60 @@ class SPHInclusiveSum(Expr):
         return self
 
     def _latex(self, printer):
-        return r"\sum_{}\left({}\right)".format(self.index, printer._print(self.function))  # TEST LATER
+        tex_func = printer._print(self.function)
+        if self.function.is_Add:
+            tex_func = f"\left({tex_func}\right)"
+        return r"\sum_{} {}".format(self.index, tex_func)  # TEST LATER
+
+class SPHExclusiveSum(Expr):
+    is_commutative = True
+    
+    def __new__(cls, expr, index, excluded):
+        return super().__new__(cls, expr, index, excluded)
+
+    @property
+    def index(self):
+        return self.args[1]
+
+    @property
+    def excluded(self):
+        return self.args[2]
+
+    @property
+    def function(self):
+        return self.args[0]
+
+    @property
+    def kind(self):
+        return self.expr.kind
+
+    @property
+    def free_symbols(self):
+        return self.function.free_symbols - {self.index}
+
+    def _eval_factor(self, **hints):
+        summand = self.function.factor(**hints)
+        if summand.is_Mul:
+            output = sift(summand.args, lambda w: w.is_commutative and not self.index in w.free_symbols)
+            return Mul(*output[True]) * self.func(Mul(*output[False]), self.index, self.excluded)
+
+        return self
+
+    def _eval_expand_basic(self, **hints):
+        summand = self.function.expand(**hints)
+        if summand.is_Add:
+            return Add(*[self.func(addend, self.index) for addend in summand.args])
+        elif isinstance(summand, MatrixBase):
+            return summand.applyfunc(lambda elem: self.func(elem, self.index, self.excluded))
+        elif summand != self.function:
+            return self.func(summand, self.index, self.excluded)
+        return self
+
+    def _latex(self, printer):
+        tex_func = printer._print(self.function)
+        if self.function.is_Add:
+            tex_func = f"\left({tex_func}\right)"
+        return f"\\sum_{self.index \\ne self.excluded} {tex_func}"  # TEST LATER
 
 TRADITIONAL_PRECEDENCE["SPHInclusiveSum"] = TRADITIONAL_PRECEDENCE["Sum"]
+TRADITIONAL_PRECEDENCE["SPHExclusiveSum"] = TRADITIONAL_PRECEDENCE["Sum"]
